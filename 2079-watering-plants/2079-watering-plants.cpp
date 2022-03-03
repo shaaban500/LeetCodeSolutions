69b2305c4280ac84abe1fcaa9dd019c031efff62
shaaban500 <47317932+shaaban500@users.noreply.github.com>
2022-03-03 19:36:48 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (83.77%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -10,16 +10,14 @@ public:
         for(int i = 0 ; i < n ; i++)
         {
             if(capacity >= plants[i])
-            {
-                capacity -= plants[i];
                 steps++;
-            }
             else
             {
                 steps += i * 2 + 1;
                 capacity = c;
-                capacity -= plants[i];
             }          
+            
+            capacity -= plants[i];
         }
         
         return steps;
