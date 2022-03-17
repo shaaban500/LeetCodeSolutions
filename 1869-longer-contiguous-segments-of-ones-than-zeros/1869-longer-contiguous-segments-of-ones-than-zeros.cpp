f00e749b3777d57335345ca8e8131767cff100ee
Mohamed Shaaban <47317932+shaaban500@users.noreply.github.com>
2022-03-18 01:48:35 +0200

    Time: 2 ms (43.21%), Space: 6.2 MB (10.30%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -5,6 +5,7 @@ public:
         int n = s.size();
         int one = 0, zero = 0;
         int mxOne = 0, mxZero = 0;
+        
         for(int i = 0 ; i < n ; i++)
         {
             if(s[i] == '1')
