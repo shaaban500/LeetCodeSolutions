4f3be13c3c21c81c2ea2574b16d213e8c195ddd7
Mohamed Shaaban <47317932+shaaban500@users.noreply.github.com>
2022-03-18 20:25:06 +0200

    Time: 1146 ms (5.04%), Space: 101.5 MB (49.66%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool can(int mid, vector<int>& dist, double hour)
+    {
+        int sum = 0;
+        int n = dist.size();
+        for(int i = 0 ; i < n - 1 ; i++)
+            sum += dist[i] / mid + (dist[i] % mid != 0);
+        
+        double tot = 1.0 * sum + 1.0 * dist[n - 1] / mid;
+        
+        return tot <= hour;
+    }
+    
+    int minSpeedOnTime(vector<int>& dist, double hour)
+    {
+        int l = 1, r = 1e8;
+        for(int i = 0 ; i < 50 ; i++)
+        {
+            int mid = (l + r) / 2;
+            (can(mid, dist, hour))? r = mid : l = mid;
+        }
+        
+        return r == 1e8 ? -1 : r;
+    }
+};
