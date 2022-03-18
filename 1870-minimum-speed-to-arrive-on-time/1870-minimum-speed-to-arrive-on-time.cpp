15bf990776897152f7fde782ce131fe9590e92cf
Mohamed Shaaban <47317932+shaaban500@users.noreply.github.com>
2022-03-18 20:53:27 +0200

    Time: 678 ms (5.04%), Space: 101.5 MB (49.66%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -16,15 +16,15 @@ public:
     int minSpeedOnTime(vector<int>& dist, double hour)
     {
         int l = 1, r = 1e8;
-        int ans = -1;
-        while(l <= r)
+        
+        while(l < r)
         {
             int mid = (l + r) / 2;
-            if(can(mid, dist, hour)) {ans = mid, r = mid - 1;}else l = mid + 1;    
+            (can(mid, dist, hour))? r = mid : l = mid + 1;    
             cout << l << " "<<r<<endl;
         }
         
-        return ans;
+        return r == 1e8 ? -1 : r;
     }
 };
 
@@ -38,27 +38,3 @@ public:
         }
         
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
