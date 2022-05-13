ed8a759ccbe04248c3527fd82767280b5634be58
Mohamed Shaaban <47317932+shaaban500@users.noreply.github.com>
2022-05-13 18:15:07 +0200

    Time: 391 ms (43.40%), Space: 22.2 MB (56.03%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dp[100][100][200];
+    bool hasValidPath(vector<vector<char>>& grid) 
+    {
+        memset(dp, -1, sizeof dp);
+        return solve(0, 0, 0, 0, grid);
+    }
+    
+    bool solve(int i, int j, int open, int closed, vector<vector<char>>& grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        int dif = open - closed;
+        
+        if(dif < 0)
+            return false;
+            
+        if(i == n - 1 && j == m - 1)
+        {
+            int o = open + (grid[i][j] == '(');
+            int c = closed + (grid[i][j] == ')');
+            dif = o - c;
+            return (dif == 0);
+        }
+        if(i >= n || j >= m) 
+            return false;
+
+        if(dp[i][j][dif] != -1)
+            return dp[i][j][dif];
+        
+        return dp[i][j][dif] =
+            solve(i + 1, j, open + (grid[i][j] == '('), closed + (grid[i][j] == ')'), grid)
+          || solve(i, j + 1, open + (grid[i][j] == '('), closed + (grid[i][j] == ')'), grid);
+    }
+     
+    
+};
