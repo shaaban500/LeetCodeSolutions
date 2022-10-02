a54594e98694da84dacb8f6f6eeb9dddc786fdc6
Mohamed Shaaban <47317932+shaaban500@users.noreply.github.com>
2022-10-03 01:41:11 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (100.00%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     bool equalFrequency(string word) 
     {
-        int n = word.size();
-        vector<int> freq(26, 0);
         int d = 0;
-        for(int i = 0 ; i < n ; i++)
-        {
-            freq[word[i] - 'a']++;
-            if(freq[word[i] - 'a'] == 1)
-                d++;
-        }
+        vector<int> freq(26, 0);
+        
+        for(int i = 0 ; i < word.size() ; i++)
+            d += ++freq[word[i] - 'a'] == 1;
        
-        int cnt = 0;
-        bool f = 0;
+        set<int>st;
         for(int i = 0 ; i < 26 ; i++)
         {
-            f = 1;
             freq[i]--;
-            set<int>st;
+            st.clear();
+            
             for(int j = 0 ; j < 26 ; j++)
             {
-                if(freq[j]) st.insert(freq[j]);    
-            }
-            
+                if(freq[j])
+                    st.insert(freq[j]);
+            }    
+
             if(st.size() == 1)
                 return true;
-            st.clear();
+            
             freq[i]++;
         }
-        
+
         return false;
-        
     }
 };
